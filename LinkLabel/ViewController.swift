ba6e6820cbaf99819e047b11e6bb47b6981aa19b
wujianguo <lsjustin89@gmail.com>
2016-09-06 11:45:50 +0800

    init

---
+++ LinkLabel/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  LinkLabel
+//
+//  Created by wujianguo on 16/9/6.
+//  Copyright © 2016年 wujianguo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
